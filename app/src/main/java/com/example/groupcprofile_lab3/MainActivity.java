e6ae95ebe9dbcaf42789e3098022125580566968
John Rey <42550367+SirPancakes@users.noreply.github.com>
2021-02-08 17:33:06 -0800

    Project Layout Done, Added Profile Activities for all members to work on - February 08, 2021 - 5:27pm

--- app/src/main/java/com/example/groupcprofile_lab3/MainActivity.java
+++ app/src/main/java/com/example/groupcprofile_lab3/MainActivity.java
@@ -18,4 +18,19 @@ public class MainActivity extends AppCompatActivity {
         Intent profile_johnrey_intent = new Intent(this, ProfileJohnReyActivity.class);
         startActivity(profile_johnrey_intent);
     }
+
+    public void goToProfile_Mohit(View view) {
+        Intent profile_mohit_intent = new Intent(this, ProfileMohitActivity.class);
+        startActivity(profile_mohit_intent);
+    }
+
+    public void goToProfile_Daniel(View view) {
+        Intent profile_daniel_intent = new Intent(this, ProfileDanielActivity.class);
+        startActivity(profile_daniel_intent);
+    }
+
+    public void goToProfile_Daniyal(View view) {
+        Intent profile_daniyal_intent = new Intent(this, ProfileDaniyalActivity.class);
+        startActivity(profile_daniyal_intent);
+    }
 }
--- app/src/main/java/com/example/groupcprofile_lab3/ProfileDaniyalActivity.java
+++ app/src/main/java/com/example/groupcprofile_lab3/ProfileDaniyalActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class DaniyalProfileActivity extends AppCompatActivity {
+public class ProfileDaniyalActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
