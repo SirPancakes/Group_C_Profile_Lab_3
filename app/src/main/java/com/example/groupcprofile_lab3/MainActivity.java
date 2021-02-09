0aa02b37b99c914c034a8fbc642708799a00e932
John Rey <42550367+SirPancakes@users.noreply.github.com>
2021-02-08 17:00:07 -0800

    Initial commit

---
+++ app/src/main/java/com/example/groupcprofile_lab3/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.groupcprofile_lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToProfile_JohnRey(View view) {
+        Intent profile_johnrey_intent = new Intent(this, ProfileJohnReyActivity.class);
+        startActivity(profile_johnrey_intent);
+    }
+}
---
+++ app/src/main/java/com/example/groupcprofile_lab3/ProfileJohnReyActivity.java
@@ -0,0 +1,14 @@
+package com.example.groupcprofile_lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileJohnReyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_john_rey);
+    }
+}
