e87a3568c7da79ecf1c7ddb3fbf58f4dac6a1a4a
John Rey <42550367+SirPancakes@users.noreply.github.com>
2021-02-08 17:24:07 -0800

    Project Layout Done, Added Profile Activities for all members to work on - February 08, 2021

---
+++ app/src/main/java/com/example/groupcprofile_lab3/DaniyalProfileActivity.java
@@ -0,0 +1,14 @@
+package com.example.groupcprofile_lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DaniyalProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daniyal_profile);
+    }
+}
